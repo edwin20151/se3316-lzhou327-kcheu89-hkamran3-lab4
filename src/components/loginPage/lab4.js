526d5840f9aa986c1b06639b1a14d37fa0b690ac
LiQian Zhou <lzhou327@uwo.ca>
2022-11-18 23:08:00 -0500

    sign up

--- src/components/loginPage/lab4.js
+++ src/components/loginPage/lab4.js
@@ -1,5 +1,6 @@
 document.getElementById("loginButton").addEventListener("click", login);
 document.getElementById("signupButton").addEventListener("click", signup);
+document.getElementById("changepasswordButton").addEventListener("click", changePassword);
 function login() {
   const newlist = {
     username: document.getElementById("user-f").value,
@@ -36,7 +37,7 @@ function signup() {
     password: document.getElementById("password-f").value,
     email: document.getElementById("email-f").value,
   };
-  fetch("/account/signup", {
+  fetch("/account", {
     method: "POST",
     headers: { "content-type": "application/json" },
     body: JSON.stringify(newlist),
@@ -47,15 +48,31 @@ function signup() {
 
         console.log("ok");
         document.getElementById("list").innerText = "sucess";
-      } else if (res.status == 401) {
-        console.log("Error: ", res.status);
-        document.getElementById("list").innerText =
-          "please contact the site administrator";
-      } else {
-        console.log("Error: ", res.status);
-        document.getElementById("list").innerText =
-          "Wrong password / usernames";
-      }
-    })
-    .catch();
-}
+      } else 
+          "something username";
+    }
+)}
+
+/*function changePassword() {
+
+    const username = document.getElementById("user-f").value,
+    const password =  document.getElementById("password-f").value,
+    const email = document.getElementById("email-f").value,
+
+  fetch("/", {
+    method: "PATCH",
+    headers: { "content-type": "application/json" },
+    body: JSON.stringify(newlist),
+  }
+    .then((res) => {
+      if (res.ok) {
+        res.json();
+
+        console.log("ok");
+        document.getElementById("list").innerText = "sucess";
+      } else 
+          "something username";
+    }
+))}*/
+
+
