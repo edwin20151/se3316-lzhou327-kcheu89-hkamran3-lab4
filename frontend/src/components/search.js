ede7b97118ed60cd0a291f12d951cbaa3fd274b5
LiQian Zhou <90808836+edwin20151@users.noreply.github.com>
2022-11-25 00:12:11 -0500

    search work perfect

--- frontend/src/components/search.js
+++ frontend/src/components/search.js
@@ -4,111 +4,140 @@ import axios from 'axios';
 
 export default class Search extends Component {
 
-    onSubmit(a){
-        a.preventDefault();
-        this.TrackSearch = (e)=>{
-            const track = e.currentTarget.value;
-            axios.get("http://localhost:5500/track/track/"+track)
-            .then(res => {
-                console.log(res.data)
-                const l = document.getElementById('list');
+    
+    constructor(props) {
+        super(props);
+        this.onChangealbum = this.onChangealbum.bind(this);
+        this.onChangeartist = this.onChangeartist.bind(this);
+        this.onChangegenres = this.onChangegenres.bind(this);
+        this.onChangetrack = this.onChangetrack.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
+    
+        this.state = {
+            track_title: "",
+            album_title: "",
+            artist_name: "",
+            track_genres: "",
+        };
+      }
+
+
+      Search() {
+        const track = {
+            track_title: this.state.track_title,
+            album_title: this.state.album_title,
+            artist_name: this.state.artist_name,
+            track_genres: this.state.track_genres,
+        };
+    
+        axios
+          .post("http://localhost:5500/track/search", track)
+          .then(res => {
+            console.log(res.data)
+            const l = document.getElementById('list');
+            document.getElementById("expand").addEventListener('click', expand);
+            function expand(){
                 res.data.forEach(e => {
                     const item = document.createElement('li');
-                    item.appendChild(document.createTextNode(` trickname: ${e.track_title}, artistname:  ${e.artist_name}`))
+                    item.appendChild(document.createTextNode(` trickname: ${e.track_genres}`))
                     l.appendChild(item);
-                });
-              
-                });
-            }
-        }
-    
+                })}
+            res.data.forEach(e => {
+                const item = document.createElement('li');
+                item.appendChild(document.createTextNode(` trickname: ${e.track_title}, artistname:  ${e.artist_name}`))
+                l.appendChild(item);
 
-   
-   
-
-render() {
-    return(
-        
-        <div id="background" className="backgroundContainer">
-        <div id="form" className="formContainer">
-          <h1 id="search">search result</h1>
+            
+            });
           
-           
-              <input
-                type="text"
-                name="artist"
-                id="artist-f"
-                class="artist-f-input"
-                placeholder="artist"
-            
-              />
-
-                <div class="buttonContainer">
-                <input
-                  value="Searchartist"
-                  className="btn btn-primary"
-                />
-                </div>
-                <br />
-
-
-              <br />
-              <input
-                type="text"
-                name="genre"
-                id="genre"
-                class="genre-f-input"
-                placeholder="genre"
-             
-              />
-              <div class="buttonContainer">
-                <input
-                  value="Searchgenre"
-                  className="btn btn-primary"
-                />
-                </div>
-               <br />
-              <input
-                type="text"
-                name="band"
-                id="band-f"
-                class="band-f-input"
-                placeholder="band"
-               
-              />
-              <div class="buttonContainer">
-                <input
-                  value="Searchband"
-                  className="btn btn-primary"
-                />
-                </div>
-               <br />
-
-               <form onSubmit={this.onSubmit}>
-               <input
-                type="Search"
-                name="track"
-                id="track-f"
-                class="track-f-input"
-                placeholder="track"
-                //value={value}
-                onChange={this.TrackSearch}
-              />
-              <div class="buttonContainer">
-                <input
-                  type="submit"
-                  value="search"
-                  className="btn btn-primary"
-                />
-              </div>
-
-              </form>
-
-            
-          <ol id="list"></ol>
-        </div>
-      </div>
+            });
+        }
       
-    )
-  }
+    
+      onChangealbum(e) {
+        this.setState({
+            album_title: e.target.value,
+        });
+      }
+      onChangetrack(e) {
+        this.setState({
+            track_title: e.target.value,
+        });
+      }
+      onChangegenres(e) {
+        this.setState({
+            track_genres: e.target.value,
+        });
+      }
+      onChangeartist(e) {
+        this.setState({
+            artist_name: e.target.value,
+        });
+      }
+    
+      onSubmit(e) {
+        e.preventDefault();
+        this.Search();
+        
+      }
+    
+      render() {
+        return (
+          <div id="background" className="backgroundContainer">
+            <div id="form" className="formContainer">
+              <h1 id="login">Search </h1>
+              <form onSubmit={this.onSubmit}>
+                  <input
+                    type="search"
+                    name="artist"
+                    id="artist-f"
+                    class="artist-f-input"
+                    placeholder="artist"
+                    value={this.state.artist}
+                    onChange={this.onChangeartist}
+                  />
+                  <input
+                    type="search"
+                    name="genres"
+                    id="genres-f"
+                    class="genres-f-input"
+                    placeholder="genres"
+                    value={this.state.genres}
+                    onChange={this.onChangegenres}
+                  />
+                  <input
+                    type="search"
+                    name="album"
+                    id="album-f"
+                    class="album-f-input"
+                    placeholder="album"
+                    value={this.state.album}
+                    onChange={this.onChangealbum}
+                  />
+                  <input
+                  type="search"
+                  name="track"
+                  id="track-f"
+                  class="track-f-input"
+                  placeholder="track"
+                  value={this.state.track}
+                  onChange={this.onChangetrack}
+                />
+                  <div class="buttonContainer">
+                    <input
+                      type="submit"
+                      value="search"
+                      className="btn btn-primary"
+                    />
+                    </div>
+              </form>
+              <ol id="list"></ol>
+              <br />
+              <div class="buttonContainer">
+                  <button id="expand"> expand</button>
+                  </div>
+            </div>
+          </div>
+        );
+      }
 }
