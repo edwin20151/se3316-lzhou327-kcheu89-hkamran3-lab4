926811dcc330ae1cfe52b9e2d54942ba1ce91a56
Nico Cheung <kcheu89@uwo.ca>
2022-11-26 21:22:27 -0400

    Get data for admin page

--- frontend/src/components/admin.js
+++ frontend/src/components/admin.js
@@ -1,15 +1,42 @@
-import React, { Component } from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
 
-export default class Admin extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      name: "",
-      email: "",
+const Admin = () => {
+  const [users, setUsers] = useState([]);
+  const [userStatus, setUserStatus] = useState(false);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await axios.get("http://localhost:5500/account/");
+      const result = await res.json();
+      console.log(result);
+      if (!result.error) {
+        setUsers(result.users);
+      } else {
+        console.log(result);
+      }
     };
-  }
-  render() {
-    return <div></div>;
-  }
-}
+    fetchData().catch(console.error);
+  }, []);
+
+  const changeeUserStatus = async (email) => {
+    try {
+      let user = {};
+      const res = await axios.patch(
+        "http://localhost:5500/account/status" + email,
+        user
+      );
+      const result = await res.json();
+      if (!result.error) {
+      } else {
+        console.log(result.error);
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  return <></>;
+};
+
+export default Admin;
