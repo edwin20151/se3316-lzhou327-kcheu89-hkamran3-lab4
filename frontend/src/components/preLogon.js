39c45a5dc7e7b6c23ad2be482bbbd47c8edf44d3
Nico Cheung <kcheu89@uwo.ca>
2022-12-01 01:00:40 -0500

    format about

--- frontend/src/components/preLogon.js
+++ frontend/src/components/preLogon.js
@@ -91,13 +91,16 @@ export default class PreLogon extends Component {
       <div>
         <h2>Lab4 Music Web</h2>
         <div className="App">
-          Our Website is here to help users experience the best possible musical
-          experience in the most organized manner. We allow you to access
-          artists, albums, tracks and genres as well as being able to create a
-          custom list of tracks. Our goal is for the user to be able to do
-          whatever function they want when it pertains to music, whether thats
-          giving their opinions or saving their favourite songs. We are here to
-          stay and we hope you join us in the journey.
+          <p>
+            Our Website is here to help users experience the best possible
+            musical experience in the most organized manner. We allow you to
+            access artists, albums, tracks and genres as well as being able to
+            create a custom list of tracks. Our goal is for the user to be able
+            to do whatever function they want when it pertains to music, whether
+            thats giving their opinions or saving their favourite songs. We are
+            here to stay and we hope you join us in the journey.
+          </p>
+          <br />
         </div>
         <h3>Public Playlist</h3>
         <table className="table">
