6593c8f82ccb3da41375a15deca593ba579ed1db
hkamran3 <90798557+hkamran3@users.noreply.github.com>
2022-12-02 13:37:35 -0500

    Small edit

---
+++ backend/account.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const AccountSchema = new Schema({
+  username: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  account: {
+    type: Boolean,
+    require: true,
+  },
+  confirmed: {
+    type: Boolean,
+    default: false,
+  },
+  confirmationCode: {
+    type: String,
+    unique: true,
+  },
+});
+module.exports = mongoose.model("account", AccountSchema);
